cd4599a19959eaa7dc40e90d006ebfd4cd6f8403
kerwan0916 <jkevin.fuss@gmail.com>
2023-02-09 18:24:51 -0500

    UpdatedRevisionPython

--- apipull.py
+++ apipull.py
@@ -1,13 +1,21 @@
 
-guns = [
+"""hash(guns) = [
     pistols: classic, shorty, frenzy, ghost, sheriff
     smgs: stinger, spectre
     shotguns: bucky, judge
     rifles: bulldog, guardian, phantom, vandal
     snipers: marshall, operator
     lmgs: ares, odin
-]
+]"""
+
+import random
+
+def momoAttaquant(oui, momo):
+    if oui >= 54:
+        temp = 5 * random()+1
+        return momo % temp
+
+print(momoAttaquant(59,12))
 
 
-#def valorantGuns(gun, skin):
     
