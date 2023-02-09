236199ef1bc4c335f6d66b3132c2b5bee9180541
kerwan0916 <jkevin.fuss@gmail.com>
2023-02-09 18:23:58 -0500

    RevisionPython

---
+++ apipull.py
@@ -0,0 +1,13 @@
+
+guns = [
+    pistols: classic, shorty, frenzy, ghost, sheriff
+    smgs: stinger, spectre
+    shotguns: bucky, judge
+    rifles: bulldog, guardian, phantom, vandal
+    snipers: marshall, operator
+    lmgs: ares, odin
+]
+
+
+#def valorantGuns(gun, skin):
+    
