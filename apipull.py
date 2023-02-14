27d1534b115aa5ab9adc754d743ed3d65b95a1b2
kerwan0916 <jkevin.fuss@gmail.com>
2023-02-14 12:01:46 -0500

    removed useless code

--- apipull.py
+++ apipull.py
@@ -1,21 +1,15 @@
 
-"""hash(guns) = [
+"""
     pistols: classic, shorty, frenzy, ghost, sheriff
     smgs: stinger, spectre
     shotguns: bucky, judge
     rifles: bulldog, guardian, phantom, vandal
     snipers: marshall, operator
     lmgs: ares, odin
-]"""
+"""
 
 import random
 
-def momoAttaquant(oui, momo):
-    if oui >= 54:
-        temp = 5 * random()+1
-        return momo % temp
-
-print(momoAttaquant(59,12))
 
 
     
